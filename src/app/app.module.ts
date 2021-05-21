7d9268c251d30255ea1403686304d29a9115970f
NITEESH <niteeshvijayakumar500@gmail.com>
2021-05-21 11:17:41 +0530

    'reactiveforms_1'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 
 import { AppComponent } from './app.component';
@@ -57,12 +65,19 @@ import { LoginComponent } from './login/login.component';
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
-    MatDialogModule 
+    MatDialogModule ,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule ,
+    ReactiveFormsModule,
+    MatSelectModule ,
+    MatSlideToggleModule
     
   ],
   
   providers: [DishService,PromotionService,LeaderService],
-  
+
   entryComponents: [
     LoginComponent
 ],
